432cc9f77246ebc2ccdabda88ceb6fb1934c167d
Yaasir024 <yope4ever@gmail.com>
2022-12-02 17:23:05 +0100

    ?Preview Component Resolver

---
+++ src/components/Editor/PreviewComponentResolver.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Layout01 from "@/components/Editor/Main/Layout/01.vue";
+import Layout02 from "@/components/Editor/Main/Layout/02.vue";
+import Layout03 from "@/components/Editor/Main/Layout/03.vue";
+import Layout04 from "@/components/Editor/Main/Layout/04.vue";
+import Layout05 from "@/components/Editor/Main/Layout/05.vue";
+
+const props = defineProps(["data"]);
+
+const layouts = {
+  Layout01,
+  Layout02,
+  Layout03,
+  Layout04,
+  Layout05,
+};
+</script>
+
+<template>
+  <component
+    :is="layouts[data.design.layout.layout]"
+    class="tab"
+    :data="data"
+  ></component>
+</template>
