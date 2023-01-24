1a7298d2c3199397911a04da423cf7b78f693fb2
Yaasir024 <yope4ever@gmail.com>
2023-01-24 21:40:30 +0100

    /header

--- src/components/Navigations/Navbar.vue
+++ src/components/Navigations/Navbar.vue
@@ -257,27 +257,37 @@ const signOut = () => {
         </transition>
       </div>
     </div>
-    <div
-      class="flex md:hidden items-center justify-center cursor-pointer"
-      @click="toggleNav"
-      ref="mobileNav"
-    >
-      <svg
-        clip-rule="evenodd"
-        fill-rule="evenodd"
-        stroke-linejoin="round"
-        stroke-miterlimit="2"
-        viewBox="0 0 24 24"
-        height="26"
-        width="26"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
+    <div class="flex md:hidden items-center ">
+      <RouterLink to="/signup" class="mr-2" v-if="!useAuth.userState">
+        <button
+          class="py-2 px-4 bg-primary-color text-white font-medium rounded-lg"
+        >
+          Create Account
+        </button>
+      </RouterLink>
+
+      <div
+        class="cursor-pointer"
+        @click="toggleNav"
+        ref="mobileNav"
       >
-        <path
-          d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z"
-          fill-rule="nonzero"
-        />
-      </svg>
+        <svg
+          clip-rule="evenodd"
+          fill-rule="evenodd"
+          stroke-linejoin="round"
+          stroke-miterlimit="2"
+          viewBox="0 0 24 24"
+          height="26"
+          width="26"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z"
+            fill-rule="nonzero"
+          />
+        </svg>
+      </div>
     </div>
   </nav>
 
